c5da916e5af18d5320fde47b65a2a41b0959d581
weels <604403450@qq.com>
2017-04-23 15:49:21 +0800

    '分离前端开发环境'

--- build/webpack.development.js
+++ build/webpack.development.js
@@ -24,7 +24,7 @@ var devConfig = merge(baseWebpackConfig, {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            filename: '../views/index.html',
+            filename: 'index.html',
             template: 'index.html',
             inject: true
         }),
