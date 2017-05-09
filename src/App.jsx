ff053fef6a854f255048b2fe5dc1610ab33040db
weels <604403450@qq.com>
2017-05-09 10:46:10 +0800

    '完成布局页'

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import { Button } from 'react-toolbox/lib/button';
+import styles from './styles/site';
 
 export default class App extends Component {
     render() {
         return (
-            <div className="title">
-                <h1>This is react!</h1>
-                {this.props.children}
+            <div className={styles.wrap}>
+                <header className={styles.header}>
+                    <div className={styles.center}>
+                        <span>WEELS</span>
+                    </div>
+                </header>
+                <div className={styles.main}>
+                    <div className={styles.center}>
+                        {this.props.children}
+                    </div>
+                </div>
+                <footer className={styles.footer}>
+                    <div className={styles.center}>
+                        <p className={styles.copyright}>created by weels</p>
+                    </div>
+                </footer>
             </div>
         );
     }
