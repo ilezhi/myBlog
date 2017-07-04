c61d2bf85b9d91f7497ae2d2f21b903d18a5ea40
wmm <mmdongme@gmail.com>
2017-07-04 17:21:07 +0800

    '添加标签api'

--- routes/index.js
+++ routes/index.js
@@ -3,26 +3,12 @@ var fs = require('fs');
 var article = require('../controllers/article');
 var tag = require('../controllers/tag');
 
-router.get('/', async (ctx, next) => {
-  ctx.body = {
-    code: 200,
-    msg: '成功'
-  };
-});
-
-router.post('/', async (ctx, next) => {
-  console.log(ctx.request.body);
-  ctx.body = {
-    code: 200,
-    msg: 'success'
-  };
-});
-
 router.get('/article/list', article.list);
 router.post('/article/save', article.save);
 router.post('/article/del', article.del);
 
 
 router.post('/tag/create', tag.create);
+router.get('/tag/list', tag.list);
 
 module.exports = router;
